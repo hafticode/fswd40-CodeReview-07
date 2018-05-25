f0537d566295d44003a12a757d75d5c8e4686bfc
Teresa Havlicek <teresahavlicek@Teresas-MacBook-Pro.local>
2018-05-25 19:57:38 +0200

    final

--- script.js
+++ script.js
@@ -16,80 +16,62 @@ var sight = /** @class */ (function () {
         this.address = address;
         this.image = image;
     }
-    return sight;
-}());
-var karlskirche = new sight("St. Charles Church", "Vienna", 1010, "Karslplatz 1", "img/karlskirche.jpg");
-var zoo = new sight("Zoo Vienna", "Vienna", 1130, "Maxingstraße 13b", "img/zoo.jpg");
-var places = [];
-places.push(karlskirche, zoo);
-function render() {
-    for (i = 0; i < places.length; i++) {
+    sight.prototype.render = function () {
         var row = document.getElementById("row1");
-        var dummy = document.createElement("div");
-        dummy.classList.add("col-lg-3");
-        dummy.classList.add("col-md-6");
-        dummy.classList.add("col-sm-12");
-        dummy.classList.add("col-xs-12");
-        row.appendChild(dummy);
+        var containerDiv = document.createElement("div");
+        containerDiv.classList.add("col-lg-3");
+        containerDiv.classList.add("col-md-6");
+        containerDiv.classList.add("col-sm-12");
+        containerDiv.classList.add("col-xs-12");
+        row.appendChild(containerDiv);
         var imgdiv = document.createElement("div");
         var img = document.createElement("img");
         imgdiv.append(img);
-        $(img).attr("src", places[i].image);
+        $(img).attr("src", this.image);
         $(imgdiv).addClass("d-none").addClass("d-md-block");
-        $(dummy).append(imgdiv);
-        var name = document.createElement("h4");
-        $(name).text(places[i].name);
-        $(dummy).append(name);
-        $(dummy).append("<p> " + places[i].address + " </p>");
-        $(dummy).append("<p> " + places[i].zipcode + " " + places[i].city + " </p>");
-    }
-}
-;
-render();
-;
+        $(containerDiv).append(imgdiv);
+        var nameHeader = document.createElement("h4");
+        $(nameHeader).text(this.name);
+        $(containerDiv).append(nameHeader);
+        $(containerDiv).append("<p> " + this.address + " </p>");
+        $(containerDiv).append("<p> " + this.zipcode + " " + this.city + " </p>");
+    };
+    return sight;
+}());
 var restaurant = /** @class */ (function (_super) {
     __extends(restaurant, _super);
-    function restaurant(name, city, zipcode, address, image, telephone, type, web) {
+    function restaurant(name, city, zipcode, address, image, telephone, cuisine, web) {
         var _this = _super.call(this, name, city, zipcode, address, image) || this;
         _this.telephone = telephone;
-        _this.type = type;
+        _this.cuisine = cuisine;
         _this.web = web;
         return _this;
     }
-    return restaurant;
-}(sight));
-var lemonleaf = new restaurant("Lemon Leaf Thai Restaurant", "Vienna", 1050, "Kettenbrückengasse 19", "img/thai.png", "+43(1)5812308", "Thai Restaurant", "www.lemonleaf.at");
-var sixta = new restaurant("SIXTA", "Vienna", 1050, "Schönbrunner Straße 21", "img/sixta.png", "+43 1 58 528 56", "Viennese Restaurant", "http://www.sixta-restaurant.at/");
-var restaurants = [];
-restaurants.push(lemonleaf, sixta);
-function renderrestaurants() {
-    for (i = 0; i < restaurants.length; i++) {
+    restaurant.prototype.render = function () {
         var row = document.getElementById("row1");
-        var dummy = document.createElement("div");
-        dummy.classList.add("col-lg-3");
-        dummy.classList.add("col-md-6");
-        dummy.classList.add("col-sm-12");
-        dummy.classList.add("col-xs-12");
-        row.appendChild(dummy);
+        var containerDiv = document.createElement("div");
+        containerDiv.classList.add("col-lg-3");
+        containerDiv.classList.add("col-md-6");
+        containerDiv.classList.add("col-sm-12");
+        containerDiv.classList.add("col-xs-12");
+        row.appendChild(containerDiv);
         var imgdiv = document.createElement("div");
         var img = document.createElement("img");
         imgdiv.append(img);
-        $(img).attr("src", restaurants[i].image);
+        $(img).attr("src", this.image);
         $(imgdiv).addClass("d-none").addClass("d-md-block");
-        $(dummy).append(imgdiv);
-        var name = document.createElement("h4");
-        $(name).text(restaurants[i].name);
-        $(dummy).append(name);
-        $(dummy).append("<p> " + restaurants[i].type + " </p>");
-        $(dummy).append("<p> " + restaurants[i].address + " </p>");
-        $(dummy).append("<p> " + restaurants[i].zipcode + " " + restaurants[i].city + " </p>");
-        $(dummy).append("<p> Phonenumber: " + restaurants[i].telephone + " </p>");
-        $(dummy).append("<p> Webaddress: " + restaurants[i].web + " </p>");
-    }
-}
-;
-renderrestaurants();
-;
+        $(containerDiv).append(imgdiv);
+        var nameHeader = document.createElement("h4");
+        $(nameHeader).text(this.name);
+        $(containerDiv).append(nameHeader);
+        $(containerDiv).append("<p> " + this.cuisine + " </p>");
+        $(containerDiv).append("<p> " + this.address + " </p>");
+        $(containerDiv).append("<p> " + this.zipcode + " " + this.city + " </p>");
+        $(containerDiv).append("<p> Phonenumber: " + this.telephone + " </p>");
+        $(containerDiv).append("<p> Webaddress: " + this.web + " </p>");
+    };
+    return restaurant;
+}(sight));
 var event = /** @class */ (function (_super) {
     __extends(event, _super);
     function event(name, city, zipcode, address, image, date, time, ticket) {
@@ -99,45 +81,39 @@ var event = /** @class */ (function (_super) {
         _this.ticket = ticket;
         return _this;
     }
-    return event;
-}(sight));
-var kristofferson = new event("Kris Kristofferson", "Vienna", 1150, "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "img/kristofferson.jpeg", "Fr., 15. Jun.", "20:00", "58,50 EUR");
-var kravitz = new event("Lenny Kravitz", "Vienna", 1150, "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/kravitz.jpg", "Sat, 09.06.2018", "19:30", "47,80 EUR");
-var events = [];
-events.push(kristofferson, kravitz);
-function renderevents() {
-    for (i = 0; i < events.length; i++) {
+    event.prototype.render = function () {
         var row = document.getElementById("row1");
-        var dummy = document.createElement("div");
-        dummy.classList.add("col-lg-3");
-        dummy.classList.add("col-md-6");
-        dummy.classList.add("col-sm-12");
-        dummy.classList.add("col-xs-12");
-        row.appendChild(dummy);
+        var containerDiv = document.createElement("div");
+        containerDiv.classList.add("col-lg-3");
+        containerDiv.classList.add("col-md-6");
+        containerDiv.classList.add("col-sm-12");
+        containerDiv.classList.add("col-xs-12");
+        row.appendChild(containerDiv);
         var imgdiv = document.createElement("div");
         var img = document.createElement("img");
         imgdiv.append(img);
-        $(img).attr("src", events[i].image);
+        $(img).attr("src", this.image);
         $(imgdiv).addClass("d-none").addClass("d-md-block");
-        $(dummy).append(imgdiv);
-        var name = document.createElement("h4");
-        $(name).text(events[i].name);
-        $(dummy).append(name);
-        $(dummy).append("<p> " + events[i].date + " " + events[i].time + " </p>");
-        $(dummy).append("<p> " + events[i].address + " </p>");
-        $(dummy).append("<p> " + events[i].zipcode + " " + places[i].city + " </p>");
-        $(dummy).append("<p> Price: " + events[i].ticket + " </p>");
-    }
-    ;
+        $(containerDiv).append(imgdiv);
+        var nameHeader = document.createElement("h4");
+        $(nameHeader).text(this.name);
+        $(containerDiv).append(nameHeader);
+        $(containerDiv).append("<p> " + this.date + " " + this.time + " </p>");
+        $(containerDiv).append("<p> " + this.address + " </p>");
+        $(containerDiv).append("<p> " + this.zipcode + " " + this.city + " </p>");
+        $(containerDiv).append("<p> Price: " + this.ticket + " </p>");
+    };
+    return event;
+}(sight));
+var karlskirche = new sight("St. Charles Church", "Vienna", 1010, "Karslplatz 1", "img/karlskirche.jpg");
+var zoo = new sight("Zoo Vienna", "Vienna", 1130, "Maxingstraße 13b", "img/zoo.jpg");
+var lemonleaf = new restaurant("Lemon Leaf Thai Restaurant", "Vienna", 1050, "Kettenbrückengasse 19", "img/thai.png", "+43(1)5812308", "Thai Cuisine", "www.lemonleaf.at");
+var sixta = new restaurant("SIXTA", "Vienna", 1050, "Schönbrunner Straße 21", "img/sixta.png", "+43 1 58 528 56", "Viennese Cuisine", "http://www.sixta-restaurant.at/");
+var kristofferson = new event("Kris Kristofferson", "Vienna", 1150, "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "img/kristofferson.jpeg", "Fr., 15. Jun.", "20:00", "58,50 EUR");
+var kravitz = new event("Lenny Kravitz", "Vienna", 1150, "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/kravitz.jpg", "Sat, 09.06.2018", "19:30", "47,80 EUR");
+var places = [];
+places.push(karlskirche, zoo, lemonleaf, sixta, kristofferson, kravitz);
+for (i = 0; i < places.length; i++) {
+    places[i].render();
 }
 ;
-renderevents();
-;
-//   let karlskirche = new sight ("St. Charles Church", "Vienna", 1010, "Karslplatz 1", "img/karlskirche.jpg");
-//   let zoo = new sight ("Zoo Vienna", "Vienna", 1130, "Maxingstraße 13b", "img/zoo.jpg");
-//   let lemonleaf = new restaurant("Lemon Leaf Thai Restaurant", "Vienna", 1050, "Kettenbrückengasse 19", "img/thai.png", "+43(1)5812308", "thai", "www.lemonleaf.at");
-//   let sixta = new restaurant ("SIXTA", "Vienna", 1050, "Schönbrunner Straße 21", "img/sixta.png", "+43 1 58 528 56", "thai", "http://www.sixta-restaurant.at/");
-//   let kristofferson = new event ("Kris Kristofferson", "Vienna", 1150, "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "img/kristofferson.jpeg", "Fr., 15. Jun.", "20:00", "58,50 EUR");
-//   let kravitz = new event ("Lenny Kravitz", "Vienna", 1150, "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/kravitz.jpg", "Sat, 09.06.2018", "19:30", "47,80 EUR");
-//   let places = []
-//   places.push(karlskirche, zoo, lemonleaf, sixta, kristofferson, kravitz);
--- script.ts
+++ script.ts
@@ -1,88 +1,66 @@
- class sight {
+class sight {
   constructor(public name, public city, public zipcode, public address, public image){
   }
- let karlskirche = new sight ("St. Charles Church", "Vienna", 1010, "Karslplatz 1", "img/karlskirche.jpg");
-  let zoo = new sight ("Zoo Vienna", "Vienna", 1130, "Maxingstraße 13b", "img/zoo.jpg");
-  
-  let places = [];
-  places.push(karlskirche, zoo);
 
- function render(){
-    for(i=0; i<places.length; i++){ 
+ render(){
     var row = document.getElementById("row1"); 
-    var dummy = document.createElement("div");
-    dummy.classList.add("col-lg-3");
-    dummy.classList.add("col-md-6");
-    dummy.classList.add("col-sm-12");
-    dummy.classList.add("col-xs-12");
-    row.appendChild(dummy);
+    var containerDiv = document.createElement("div");
+    containerDiv.classList.add("col-lg-3");
+    containerDiv.classList.add("col-md-6");
+    containerDiv.classList.add("col-sm-12");
+    containerDiv.classList.add("col-xs-12");
+    row.appendChild(containerDiv);
+
     var imgdiv = document.createElement("div");
     var img = document.createElement("img");
     imgdiv.append(img);
-    $(img).attr("src", places[i].image);
+    $(img).attr("src", this.image);
     $(imgdiv).addClass("d-none").addClass("d-md-block");
-    $(dummy).append(imgdiv);
-    var name = document.createElement("h4");
-    $(name).text(places[i].name);
-    $(dummy).append(name);
-    $(dummy).append(`<p> ${places[i].address} </p>`);
-    $(dummy).append(`<p> ${places[i].zipcode} ${places[i].city} </p>`);
+    $(containerDiv).append(imgdiv);
+
+    var nameHeader = document.createElement("h4");
+    $(nameHeader).text(this.name);
+    $(containerDiv).append(nameHeader);
+    $(containerDiv).append(`<p> ${this.address} </p>`);
+    $(containerDiv).append(`<p> ${this.zipcode} ${this.city} </p>`);
+  }
+
 }
 
-};
-
- render ();
-};
-
-
-
+ 
  class restaurant extends sight {
 
-  constructor(name, city, zipcode, address, image, telephone, type, web){
+  constructor(name, city, zipcode, address, image, telephone, cuisine, web){
     super(name, city, zipcode, address, image);
     this.telephone = telephone;
-    this.type = type;
+    this.cuisine = cuisine;
     this.web = web;
   }
 
-  let lemonleaf = new restaurant("Lemon Leaf Thai Restaurant", "Vienna", 1050, "Kettenbrückengasse 19", "img/thai.png", "+43(1)5812308", "Thai Restaurant", "www.lemonleaf.at");
-  let sixta = new restaurant ("SIXTA", "Vienna", 1050, "Schönbrunner Straße 21", "img/sixta.png", "+43 1 58 528 56", "Viennese Restaurant", "http://www.sixta-restaurant.at/");
-
-  let restaurants = [];
-  restaurants.push(lemonleaf, sixta);
-
-
- function renderrestaurants(){
-    for(i=0; i<restaurants.length; i++){ 
+  render(){
     var row = document.getElementById("row1"); 
-    var dummy = document.createElement("div");
-    dummy.classList.add("col-lg-3");
-    dummy.classList.add("col-md-6");
-    dummy.classList.add("col-sm-12");
-    dummy.classList.add("col-xs-12");
-    row.appendChild(dummy);
+    var containerDiv = document.createElement("div");
+    containerDiv.classList.add("col-lg-3");
+    containerDiv.classList.add("col-md-6");
+    containerDiv.classList.add("col-sm-12");
+    containerDiv.classList.add("col-xs-12");
+    row.appendChild(containerDiv);
     var imgdiv = document.createElement("div");
     var img = document.createElement("img");
     imgdiv.append(img);
-    $(img).attr("src", restaurants[i].image);
-   $(imgdiv).addClass("d-none").addClass("d-md-block");
-    $(dummy).append(imgdiv);
-    var name = document.createElement("h4");
-    $(name).text(restaurants[i].name);
-    $(dummy).append(name);
-    $(dummy).append(`<p> ${restaurants[i].type} </p>`);
-    $(dummy).append(`<p> ${restaurants[i].address} </p>`);
-    $(dummy).append(`<p> ${restaurants[i].zipcode} ${restaurants[i].city} </p>`);
-    $(dummy).append(`<p> Phonenumber: ${restaurants[i].telephone} </p>`);
-    $(dummy).append(`<p> Webaddress: ${restaurants[i].web} </p>`);
-
+    $(img).attr("src", this.image);
+    $(imgdiv).addClass("d-none").addClass("d-md-block");
+    $(containerDiv).append(imgdiv);
+    var nameHeader = document.createElement("h4");
+    $(nameHeader).text(this.name);
+    $(containerDiv).append(nameHeader);
+    $(containerDiv).append(`<p> ${this.cuisine} </p>`);
+    $(containerDiv).append(`<p> ${this.address} </p>`);
+    $(containerDiv).append(`<p> ${this.zipcode} ${this.city} </p>`);
+    $(containerDiv).append(`<p> Phonenumber: ${this.telephone} </p>`);
+    $(containerDiv).append(`<p> Webaddress: ${this.web} </p>`);
+  }
 }
-
-};
-
- renderrestaurants ();
-
-};
 
 
 
@@ -93,55 +71,45 @@
        this.time = time;
        this.ticket = ticket;
     }
-  let kristofferson = new event ("Kris Kristofferson", "Vienna", 1150, "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "img/kristofferson.jpeg", "Fr., 15. Jun.", "20:00", "58,50 EUR");
-  let kravitz = new event ("Lenny Kravitz", "Vienna", 1150, "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/kravitz.jpg", "Sat, 09.06.2018", "19:30", "47,80 EUR");
 
- let events = [];
-  events.push(kristofferson, kravitz);
-
- function renderevents(){
-    for(i=0; i< events.length; i++){ 
+ render(){
     var row = document.getElementById("row1"); 
-    var dummy = document.createElement("div");
-    dummy.classList.add("col-lg-3");
-    dummy.classList.add("col-md-6");
-    dummy.classList.add("col-sm-12");
-    dummy.classList.add("col-xs-12");
-    row.appendChild(dummy);
+    var containerDiv = document.createElement("div");
+    containerDiv.classList.add("col-lg-3");
+    containerDiv.classList.add("col-md-6");
+    containerDiv.classList.add("col-sm-12");
+    containerDiv.classList.add("col-xs-12");
+    row.appendChild(containerDiv);
     var imgdiv = document.createElement("div");
     var img = document.createElement("img");
     imgdiv.append(img);
-    $(img).attr("src", events[i].image);
+    $(img).attr("src", this.image);
     $(imgdiv).addClass("d-none").addClass("d-md-block");
-    $(dummy).append(imgdiv);
-    var name = document.createElement("h4");
-    $(name).text(events[i].name);
-    $(dummy).append(name);
-    $(dummy).append(`<p> ${events[i].date} ${events[i].time} </p>`);
-    $(dummy).append(`<p> ${events[i].address} </p>`);
-    $(dummy).append(`<p> ${events[i].zipcode} ${places[i].city} </p>`);
-    $(dummy).append(`<p> Price: ${events[i].ticket} </p>`);
-  };
-};
-renderevents();
-
-  };
+    $(containerDiv).append(imgdiv);
+    var nameHeader = document.createElement("h4");
+    $(nameHeader).text(this.name);
+    $(containerDiv).append(nameHeader);
+    $(containerDiv).append(`<p> ${this.date} ${this.time} </p>`);
+    $(containerDiv).append(`<p> ${this.address} </p>`);
+    $(containerDiv).append(`<p> ${this.zipcode} ${this.city} </p>`);
+    $(containerDiv).append(`<p> Price: ${this.ticket} </p>`);
+  }
+}
 
  
-
-
-
-
-
-//   let karlskirche = new sight ("St. Charles Church", "Vienna", 1010, "Karslplatz 1", "img/karlskirche.jpg");
-//   let zoo = new sight ("Zoo Vienna", "Vienna", 1130, "Maxingstraße 13b", "img/zoo.jpg");
-//   let lemonleaf = new restaurant("Lemon Leaf Thai Restaurant", "Vienna", 1050, "Kettenbrückengasse 19", "img/thai.png", "+43(1)5812308", "thai", "www.lemonleaf.at");
-//   let sixta = new restaurant ("SIXTA", "Vienna", 1050, "Schönbrunner Straße 21", "img/sixta.png", "+43 1 58 528 56", "thai", "http://www.sixta-restaurant.at/");
-//   let kristofferson = new event ("Kris Kristofferson", "Vienna", 1150, "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "img/kristofferson.jpeg", "Fr., 15. Jun.", "20:00", "58,50 EUR");
-//   let kravitz = new event ("Lenny Kravitz", "Vienna", 1150, "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/kravitz.jpg", "Sat, 09.06.2018", "19:30", "47,80 EUR");
+  let karlskirche = new sight ("St. Charles Church", "Vienna", 1010, "Karslplatz 1", "img/karlskirche.jpg");
+  let zoo = new sight ("Zoo Vienna", "Vienna", 1130, "Maxingstraße 13b", "img/zoo.jpg");
+  let lemonleaf = new restaurant("Lemon Leaf Thai Restaurant", "Vienna", 1050, "Kettenbrückengasse 19", "img/thai.png", "+43(1)5812308", "Thai Cuisine", "www.lemonleaf.at");
+  let sixta = new restaurant ("SIXTA", "Vienna", 1050, "Schönbrunner Straße 21", "img/sixta.png", "+43 1 58 528 56", "Viennese Cuisine", "http://www.sixta-restaurant.at/");
+  let kristofferson = new event ("Kris Kristofferson", "Vienna", 1150, "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "img/kristofferson.jpeg", "Fr., 15. Jun.", "20:00", "58,50 EUR");
+  let kravitz = new event ("Lenny Kravitz", "Vienna", 1150, "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/kravitz.jpg", "Sat, 09.06.2018", "19:30", "47,80 EUR");
   
-//   let places = []
-//   places.push(karlskirche, zoo, lemonleaf, sixta, kristofferson, kravitz);
+  let places = [];
+  places.push(karlskirche, zoo, lemonleaf, sixta, kristofferson, kravitz);
+
+  for(i=0;i<places.length; i++){
+    places[i].render();
+  };
 
 
  
